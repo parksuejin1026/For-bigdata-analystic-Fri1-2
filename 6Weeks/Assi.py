e3506adc80340e4aa1408991d5925dda046855b3
310 <suejin1026suejin@gmail.com>
2025-10-17 09:42:17 +0900

    6주차 수업

--- 6Weeks/Assi.py
+++ 6Weeks/Assi.py
@@ -2,3 +2,31 @@
 # 이름 : 박수진
 # 학과 : 인공지능소프트웨어학과
 
+# 1. 원의 면적을 구해주는 함수
+def calculate_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+c_area = calculate_area(5.0)
+print(c_area)
+
+# 2. 정렬 프로그램
+def sort_num(n1, n2):
+    if n1 < n2:
+        return n1, n2
+    else:
+        return n2, n1
+
+print(sort_num(110, 210))
+print(sort_num(2100, 80))
+
+# 3. 사칙연산 계산기
+def calc(n1, n2):
+    return n1 + n2, n1 - n2, n1 * n2, n1 / n2
+
+n1, n2 = 200, 100
+t1, t2, t3, t4 = calc(n1, n2)
+print(n1, '+', n2 , '=', t1)
+print(n1, '-', n2 , '=', t2)
+print(n1, '*', n2 , '=', t3)
+print(n1, '/', n2 , '=', t4) 
